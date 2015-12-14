304906eb0c37a831a3a8aae3ee14886b7c81f757
Ben Rothman <croquetpro@gmail.com>
2015-12-14 09:14:56 -0800

    switched to react router

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -2,19 +2,29 @@ var React = require('react'),
     ReactDOM = require('react-dom'),
     Router = require('react-router').Router,
     Route = require('react-router').Route,
+    IndexRoute = require('react-router').IndexRoute,
     ApiUtil = require('./utils/api_util.js'),
     BenchStore = require('./stores/bench'),
     Search = require('./components/search');
 
 document.addEventListener('DOMContentLoaded', function () {
-  // var routes = (<Route path="/" component={App} >
-  //     <Route path="pokemons/:pokemonId" component={PokemonDetail}>
-  //       <Route path="toys/:toyId" component={ToyDetail} />
-  //     </Route>
-  //   </Route>);
+  var App = React.createClass({
+    render: function(){
+      return (
+        <div>
+          <header><h1>Bench BnB</h1></header>
+          {this.props.children}
+        </div>
+      );
+    }
+  });
+  var routes = (
+    <Route path="/" component={App} >
+      <IndexRoute component={Search} />
+    </Route>);
   var content = document.querySelector('#content');
-  ReactDOM.render(<div>
-                    <Search />
-                  </div>, content);
-  // ReactDOM.render(<Router>{routes}</Router>, content);
+  // ReactDOM.render(<div>
+  //                   <Search />
+  //                 </div>, content);
+  ReactDOM.render(<Router>{routes}</Router>, content);
 });
